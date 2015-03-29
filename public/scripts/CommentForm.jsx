ac2da529c9154f53c55165eaa361226097f7f9f3
Thanh Ky Quan <thanhquanky@gatech.edu>
2015-03-29 05:41:57 -0400

    First init

---
+++ public/scripts/CommentForm.jsx
@@ -0,0 +1,9 @@
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+                Comment form
+            </div>
+        )
+    }
+})
